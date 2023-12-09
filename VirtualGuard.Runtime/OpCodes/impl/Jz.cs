47fa3c102907cdaa41ee6521114240a9065067a1
mito <78819172+mitoiscool@users.noreply.github.com>
2023-12-09 14:19:21 -0500

    Added conditional and array handlers.

---
+++ VirtualGuard.Runtime/OpCodes/impl/Jz.cs
@@ -0,0 +1,14 @@
+using VirtualGuard.Runtime.Execution;
+
+namespace VirtualGuard.Runtime.OpCodes.impl
+{
+    public class Jz : IOpCode
+    {
+        public void Execute(VMContext ctx, out ExecutionState state)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public byte GetCode() => 0;
+    }
+}
