676cd4ab7ab04f1575f8940452afc83187709ca0
mitochondria <78819172+mitoiscool@users.noreply.github.com>
2024-03-13 16:55:12 -0400

    Fix stind and impl casting of variants

--- VirtualGuard.Runtime/OpCodes/impl/Sizeof.cs
+++ VirtualGuard.Runtime/OpCodes/impl/Sizeof.cs
@@ -1,4 +1,6 @@
+using System.Reflection;
 using System.Reflection.Emit;
+using VirtualGuard.Runtime.Variant.ValueType.Numeric;
 
 namespace VirtualGuard.Runtime.OpCodes.impl;
 
@@ -9,12 +11,22 @@ public class Sizeof : IOpCode
     {
         if (_cachedFactory == null)
         {
+            _cachedFactory = new DynamicMethod("virtualguard", typeof(int), new[] { typeof(Type) });
+            var gen = _cachedFactory.GetILGenerator();
             
+            gen.Emit(System.Reflection.Emit.OpCodes.Ldarg_0);
+            gen.Emit(System.Reflection.Emit.OpCodes.Sizeof);
+            gen.Emit(System.Reflection.Emit.OpCodes.Ret);
         }
-        
-        
 
-        _cachedFactory.Invoke(null, new[] { });
+        var type = ctx.ResolveType(ctx.Reader.ReadInt());
+
+        var size = (int)_cachedFactory.Invoke(null, new[] { type });
+        
+        ctx.Stack.Push(new IntVariant(size));
+        
+        ctx.CurrentCode += ctx.Reader.ReadFixupValue();
+        CodeMap.LookupCode(ctx.CurrentCode).Execute(ctx);
     }
 
     public byte GetCode() => 0;
