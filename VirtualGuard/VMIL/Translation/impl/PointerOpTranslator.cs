1a35c4fc6defe20f95a678a8941a269ebb43da2a
mito <78819172+mitoiscool@users.noreply.github.com>
2024-03-14 09:23:11 -0400

    Implement translation of ptr ops

--- VirtualGuard/VMIL/Translation/impl/PointerOpTranslator.cs
+++ VirtualGuard/VMIL/Translation/impl/PointerOpTranslator.cs
@@ -9,7 +9,81 @@ internal class PointerOpTranslator : ITranslator
 {
     public void Translate(AstExpression instr, ControlFlowNode<CilInstruction> node, VmBlock block, VmMethod meth, VirtualGuardContext ctx)
     {
-        throw new NotImplementedException();
+        switch (instr.OpCode.Code)
+        {
+            case CilCode.Stind_I:
+                block.WithContent(new VmInstruction(VmCode.Stind, ctx.Module.CorLibTypeFactory.IntPtr.ToTypeDefOrRef()));
+                break;
+            case CilCode.Stind_I1:
+                block.WithContent(new VmInstruction(VmCode.Stind, ctx.Module.CorLibTypeFactory.SByte.ToTypeDefOrRef()));
+                break;
+            case CilCode.Stind_I2:
+                block.WithContent(new VmInstruction(VmCode.Stind, ctx.Module.CorLibTypeFactory.Int16.ToTypeDefOrRef()));
+                break;
+            case CilCode.Stind_I4:
+                block.WithContent(new VmInstruction(VmCode.Stind, ctx.Module.CorLibTypeFactory.Int32.ToTypeDefOrRef()));
+                break;
+            case CilCode.Stind_I8:
+                block.WithContent(new VmInstruction(VmCode.Stind, ctx.Module.CorLibTypeFactory.Int64.ToTypeDefOrRef()));
+                break;
+            case CilCode.Stind_R4:
+                block.WithContent(new VmInstruction(VmCode.Stind, ctx.Module.CorLibTypeFactory.Single.ToTypeDefOrRef()));
+                break;
+            case CilCode.Stind_R8:
+                block.WithContent(new VmInstruction(VmCode.Stind, ctx.Module.CorLibTypeFactory.Double.ToTypeDefOrRef()));
+                break;
+            case CilCode.Stind_Ref:
+                block.WithContent(new VmInstruction(VmCode.Stind, ctx.Module.CorLibTypeFactory.Object.ToTypeDefOrRef()));
+                break;
+            
+            case CilCode.Ldind_I:
+                block.WithContent(new VmInstruction(VmCode.Ldind, ctx.Module.CorLibTypeFactory.IntPtr.ToTypeDefOrRef()));
+                break;
+            case CilCode.Ldind_I1:
+                block.WithContent(new VmInstruction(VmCode.Ldind, ctx.Module.CorLibTypeFactory.SByte.ToTypeDefOrRef()));
+                break;
+            case CilCode.Ldind_I2:
+                block.WithContent(new VmInstruction(VmCode.Ldind, ctx.Module.CorLibTypeFactory.Int16.ToTypeDefOrRef()));
+                break;
+            case CilCode.Ldind_I4:
+                block.WithContent(new VmInstruction(VmCode.Ldind, ctx.Module.CorLibTypeFactory.Int32.ToTypeDefOrRef()));
+                break;
+            case CilCode.Ldind_I8:
+                block.WithContent(new VmInstruction(VmCode.Ldind, ctx.Module.CorLibTypeFactory.Int64.ToTypeDefOrRef()));
+                break;
+            
+            case CilCode.Ldind_U1:
+                block.WithContent(new VmInstruction(VmCode.Ldind, ctx.Module.CorLibTypeFactory.Byte.ToTypeDefOrRef()));
+                break;
+            case CilCode.Ldind_U2:
+                block.WithContent(new VmInstruction(VmCode.Ldind, ctx.Module.CorLibTypeFactory.UInt16.ToTypeDefOrRef()));
+                break;
+            case CilCode.Ldind_U4:
+                block.WithContent(new VmInstruction(VmCode.Ldind, ctx.Module.CorLibTypeFactory.UInt32.ToTypeDefOrRef()));
+                break;
+            
+            case CilCode.Ldind_R4:
+                block.WithContent(new VmInstruction(VmCode.Ldind, ctx.Module.CorLibTypeFactory.Single.ToTypeDefOrRef()));
+                break;
+            case CilCode.Ldind_R8:
+                block.WithContent(new VmInstruction(VmCode.Ldind, ctx.Module.CorLibTypeFactory.Double.ToTypeDefOrRef()));
+                break;
+            
+            case CilCode.Ldind_Ref:
+                block.WithContent(new VmInstruction(VmCode.Ldind, ctx.Module.CorLibTypeFactory.Object.ToTypeDefOrRef()));
+                break;
+            
+            case CilCode.Stobj:
+                block.WithContent(new VmInstruction(VmCode.Stind,
+                    ctx.Module.CorLibTypeFactory.Object.ToTypeDefOrRef()));
+                break;
+            
+            case CilCode.Ldobj:
+                block.WithContent(new VmInstruction(VmCode.Ldind,
+                    ctx.Module.CorLibTypeFactory.Object.ToTypeDefOrRef()));
+                break;
+        }
+        
     }
 
     public bool Supports(AstExpression instr)
@@ -39,6 +113,10 @@ internal class PointerOpTranslator : ITranslator
             case CilCode.Ldind_Ref:
             case CilCode.Ldind_R4:
             case CilCode.Ldind_R8:
+                return true;
+            
+            case CilCode.Stobj:
+            case CilCode.Ldobj:
                 return true;
             
             default:
